ac0f077618f7d6e922e66f960ed3fdc2b8674d73
Serhey <serhey781@gmail.com>
2023-01-31 23:41:22 +0200

    version1.0.4

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 import "./header.css";
 
 const Header = () => {
@@ -8,6 +10,22 @@ const Header = () => {
         <h1 className="gradient__text">
           Let`s Build Something amazing with GPT-3 OpenAI
         </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          iure mollitia vitae labore repudiandae, odit laboriosam eum saepe
+          laudantium itaque autem? Inventore, at!
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1.600 people requsted blya</p>
+        </div>
+        <div className="qpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
       </div>
     </div>
   );
